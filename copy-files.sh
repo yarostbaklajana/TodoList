bcb711b1d3aaf925eb8b1307c1ec7ca67aa83626
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2016-11-04 12:26:22 +0200

    Changed public folder

---
+++ copy-files.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+cp -v index.html build/
+cp -v style.css build/
+cp -v main.js build/
--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,11 @@ REPO=https://yarostbaklajana:$GH_TOKEN@github.com/yarostbaklajana/TodoList.git
 ./copy-files.sh
 
 # config
-git config --global user.email "yarostbaklajana@gmail.com"
-git config --global user.name "Travis CI"
+git config --global user.email "travis"
+git config --global user.name "travis CI deploy"
 
 # deploy
-cd public
+cd build
 git init
 git add .
 git commit -m "Deployed to Github Pages"
