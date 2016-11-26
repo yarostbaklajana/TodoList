e6e85edb3b013b259573397cd771dddf0dc0f9e4
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2016-11-26 11:08:25 +0200

    Added field to Todo model that show if todo item checked; added method to View Model that toggles checking of todo item

--- src/todoList.viewModel.js
+++ src/todoList.viewModel.js
@@ -1,14 +1,23 @@
 import * as ko from 'knockout';
 
 class Todo {
-    constructor(todo) {
-        this.todo = todo;
+    constructor(value) {
+        this.value = value;
+        this.checked = ko.observable(false);
+        this.unchecked = ko.computed(() => !this.checked());
     }
+
+
+    
 }
 
 class ViewModel {
     constructor() {
-        this.todoList = [new Todo("Try javaScript"), new Todo("Buy a Unicorn")]
+        this.todoList = [new Todo("Try javaScript"), new Todo("Buy a Unicorn")]        
+    }
+
+    toggleChecking(todo) {
+        todo.checked(!todo.checked());
     }
 }
 
