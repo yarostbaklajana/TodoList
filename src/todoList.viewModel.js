8cdeb15dae497ae134e62b2105d7e518383c2e21
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2016-11-27 16:48:40 +0200

    Fixed issues after review.

--- src/todoList.viewModel.js
+++ src/todoList.viewModel.js
@@ -3,7 +3,7 @@ import {Todo} from './todo.model.js';
 
 export class ViewModel {
     constructor() {
-        this.todoList = ko.observableArray([]);        
+        this.todoList = ko.observableArray();        
         this.inputText = ko.observable("");
     }
 
@@ -11,12 +11,18 @@ export class ViewModel {
         todo.checked(!todo.checked());
     }
 
-    addNewTodo(todo, event) {
-        if(event.keyCode === 13 && this.inputText() !== '') {
+    handleKeyUp(todo, event) {
+        const ENTER_KEY_CODE = 13;
+        const ESCAPE_KEY_CODE = 27;
+        if(event.keyCode === ENTER_KEY_CODE && this.inputText()) {
             const text = this.inputText();
             const todo = new Todo(text);            
             this.todoList.unshift(todo);                
-            this.inputText("");
-        }        
+            this.inputText('');
+        } 
+
+        if (event.keyCode === ESCAPE_KEY_CODE) {
+            this.inputText('');
+        }       
     }
 }
