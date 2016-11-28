312ad081fabfeea1f9cb83269e1c9288a2591f20
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2016-11-28 18:41:51 +0200

    Added implementation of filters for TODO list

--- src/todoList.viewModel.js
+++ src/todoList.viewModel.js
@@ -1,13 +1,21 @@
 import * as ko from 'knockout';
 import {Todo} from './todo.model.js';
 
+const DISPLAY_MODS = {
+            all: 'all',
+            active: 'active',
+            completed: 'completed'
+}
+
 export class ViewModel {
-    constructor() {
+    constructor() {         
         this.todoList = ko.observableArray();        
         this.inputText = ko.observable('');
         this.itemsLeftCount = ko.computed(() => this.todoList().filter((element) => !element.checked()).length); 
         this.itemsLeftText = ko.computed(this.getItemsLeftText, this);
-    }
+        this.currentMode = ko.observable(DISPLAY_MODS.all);
+        this.displayedListOfTodos = ko.computed(this.getDisplayedTodos, this);                
+    }        
 
     toggleChecking(todo) {
         todo.checked(!todo.checked());
@@ -36,4 +44,31 @@ export class ViewModel {
         return `${count} item${ext} left`;
     }
 
+    getDisplayedTodos() {
+        const current = this.currentMode();
+
+        if (current === 'all') {
+            return this.todoList();
+        }
+
+        if (current === 'active') {
+            return this.todoList().filter((todo) => !todo.checked());
+        }
+
+        if (current === 'completed') {
+            return this.todoList().filter((todo) => todo.checked());
+        }
+    }
+
+    switchModeDisplayingAllItems() {
+        this.currentMode(DISPLAY_MODS.all);
+    }
+
+    switchModeDisplayingActiveItems() {
+        this.currentMode(DISPLAY_MODS.active);
+    }
+
+    switchModeDisplayingCompletedItems() {
+        this.currentMode(DISPLAY_MODS.completed);
+    }
 }
