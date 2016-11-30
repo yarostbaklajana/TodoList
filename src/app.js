9135aa968863392ab03b3f082131ba90dfbfd34e
Anastasiia Skorobohata <yarostbaklajana@gmail.com>
2016-11-30 23:54:02 +0200

    Fixed navigation binding

--- src/app.js
+++ src/app.js
@@ -7,18 +7,9 @@ import {ViewModel} from './todoList.viewModel.js';
 import { todosRepository } from './todosRepository.js';
 import * as pager from 'exports?pager!imports?$=jquery,ko=knockout!pagerjs';
 
-const defaultLocation = "main";
-
 const list = todosRepository.load();
 const viewModel = new ViewModel(list);
 
-
 pager.extendWithPage(viewModel);
 ko.applyBindings(viewModel);
-
-const current = window.location.hash;
-if(!current) {
-    pager.start(defaultLocation);
-} else {
-    pager.start();
-}
+pager.start();
